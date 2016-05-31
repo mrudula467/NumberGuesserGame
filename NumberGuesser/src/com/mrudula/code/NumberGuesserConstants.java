a8457cb3d9515623e9084ec6373e40b189b44e73
mrudula467 <mrudula.mrm@gmail.com>
2016-05-31 17:20:00 +0000

    Add files via upload

---
+++ NumberGuesser/src/com/mrudula/code/NumberGuesserConstants.java
@@ -0,0 +1,19 @@
+package com.mrudula.code;
+
+/**
+ * 
+ * @author Mrudula
+ * @version 1.0
+ *
+ */
+public class NumberGuesserConstants {
+
+	public static final String HIGHER = "HIGHER";
+	public static final String LOWER = "LOWER";
+	public static final String YES = "YES";
+	public static final String READY = "READY";
+
+	public static final int MIN_NUMBER = 0;
+	public static final int MAX_NUMBER = 100000;
+
+}
